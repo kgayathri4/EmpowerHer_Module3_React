1d4ef5b85c43f4f3592a69c5d3ed66223734f6d9
kgayathri4 <04gayathri.k@gmail.com>
2025-12-17 15:51:11 +0530

    react

---
+++ user-data-app/src/App.jsx
@@ -0,0 +1,12 @@
+import UserData from "./components/UserData";
+
+function App() {
+  return (
+    <div>
+      <h2>Fetch User Data Example</h2>
+      <UserData />
+    </div>
+  );
+}
+
+export default App;
