c736f2ad9bf40cf59d9a0da8c7e9b57ff53a6dc6
kgayathri4 <04gayathri.k@gmail.com>
2025-12-17 16:03:34 +0530

    react

---
+++ status-toggle-app/src/App.jsx
@@ -0,0 +1,12 @@
+import StatusToggle from "./components/StatusToggle";
+
+function App() {
+  return (
+    <div>
+      <h2>Status Toggle App</h2>
+      <StatusToggle />
+    </div>
+  );
+}
+
+export default App;
