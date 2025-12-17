725cb6d166b3c0740b47cab7d972fbadd433f2eb
kgayathri4 <04gayathri.k@gmail.com>
2025-12-17 15:38:53 +0530

    react

---
+++ calculator-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div>
+      <h2>Calculator App</h2>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
