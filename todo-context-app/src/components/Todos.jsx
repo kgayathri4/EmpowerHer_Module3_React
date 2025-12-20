fba7ccf6791774f97e95c0cab036e108a26378fd
kgayathri4 <04gayathri.k@gmail.com>
2025-12-20 17:01:03 +0530

    react

---
+++ todo-context-app/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+
+function Todos() {
+  return (
+    <div>
+      <AddTodo />
+      <TodoList />
+    </div>
+  );
+}
+
+export default Todos;
